fb4fd24ea840d51c658ad7539576aad57e537aff
Степанцев Альберт <126@gosbroker.ru>
2016-11-08 15:30:36 +0300

    + composer package support

--- src/autoload.php
+++ src/autoload.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
 spl_autoload_register(function ($class) {
     if (preg_match('~^Running~', $class)) {
         $file = __DIR__ . str_replace('\\', '/', preg_replace('~^Running~', '', $class)) . '.php';
